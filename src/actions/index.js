be85596ce51317168437af502615d469fc5cb2fa
JeanBriesemeister <jeanbr86@gmail.com>
2020-09-18 08:13:52 -0300

    Use of loadash to fetch users only once

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,15 @@
+import _ from "loadsh";
 import jsonPlaceHolder from "../apis/jsonPlaceHolder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+};
 
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceHolder.get("/posts");
@@ -17,3 +28,15 @@ export const fetchUser = (id) => async (dispatch) => {
     payload: response.data,
   });
 };
+
+//Memoize version
+/*export const fetchUser = (id) => (dispatch) => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
+  const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+  dispatch({
+    type: "FETCH_USER",
+    payload: response.data,
+  });
+});*/
